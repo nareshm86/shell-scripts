3861f2d91db9d0a455a3e2f722b204c20d5a457c
nareshm86 <nmarrimf2017@gmail.com>
2026-05-22 22:06:47 +0530

    all variables

--- specialvariables.sh
+++ specialvariables.sh
@@ -4,3 +4,6 @@ echo "All variables passed to the script: $@"
 echo "Number of variable passed: $#"
 echo "First Variable: $1
 echo "script name: $0
+echo "who is running the user: $USER"
+echo "What is working directory: $PWD"
+echo "PID of the current script: $$"
