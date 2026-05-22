894feacf25ff45309338a01607c5455454f0c54e
nareshm86 <nmarrimf2017@gmail.com>
2026-05-22 22:08:30 +0530

    all variables

--- specialvariables.sh
+++ specialvariables.sh
@@ -7,3 +7,5 @@ echo "script name: $0
 echo "who is running the user: $USER"
 echo "What is working directory: $PWD"
 echo "PID of the current script: $$"
+sleep 5 &
+echo "PID of the background command running just now: $!"
