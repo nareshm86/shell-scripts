e3cc5b4847febea06e6a6290958ff3748622906a
nareshm86 <nmarrimf2017@gmail.com>
2026-05-23 15:05:34 +0530

    all special varilables script

--- mysqlinstall.sh
+++ mysqlinstall.sh
@@ -4,17 +4,18 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
 echo "run the script from root user"
 exit 1
 fi
 
 dnf list installed mysql
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
 echo "mysql script already installed..skippig"
+else
 echo "installing the mysql"
 dnf install mysql -y
-if [$? -ne 0]; then
+if [ $? -ne 0 ]; then
 echo "Installation is failed"
 exit 1
 else
