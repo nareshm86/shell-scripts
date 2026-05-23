e0d4e1a0623b061a6d889e55c514dcd1d2977726
nareshm86 <nmarrimf2017@gmail.com>
2026-05-23 11:17:09 +0530

    all special varilables script

--- operator.sh
+++ operator.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 10 ]; then
 echo "The Given Number $NUMBER is greather than 10"
-elif [$NUMBER -eq 10 ]; then
+elif [$NUMBER = 10 ]; then
 echo "The Given Number $NUMBER is equal to 10"
 else 
 echo "The Given Number $NUMBER is lessthan 10"
