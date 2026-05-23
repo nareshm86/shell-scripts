96ef672d308d8a3f6d1a7e13dbbec801870c414e
nareshm86 <nmarrimf2017@gmail.com>
2026-05-23 15:31:46 +0530

    all special varilables script

--- mysqlinstall.sh
+++ mysqlinstall.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
 # check the user before installing
+#!/bin/bash
 
 USERID=$(id -u)
 
+# Check root access or not
 if [ $USERID -ne 0 ]; then
-echo "run the script from root user"
-exit 1
+    echo "Please run this script with root access"
+    exit 1
 fi
 
+# first arg -> what are you trying to install
+# second arg -> exit code
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is ... FAILED"
+        exit 1
+    else
+        echo "Installing $1 is ... SUCCESS"
+    fi
+}
+
+# echo "I am continuing..."
 dnf list installed mysql
+
 if [ $? -eq 0 ]; then
-   echo "mysql script already installed..skippig"
+    echo "MySQL is already installed ... SKIPPING"
 else
-   echo "installing the mysql"
-   dnf install mysql -y
- if [ $? -ne 0 ]; then
-   echo "Installation is failed"
-exit 1
- else
-echo "mysql installation is successull"
- fi
+    echo "Installing MySQL"
+    dnf install mysql -y
+    VALIDATE MySQL $?
 fi
 
+dnf list installed nginx
+if [ $? -eq 0 ]; then
+    echo "nginx is already installed ... SKIPPING"
+else
+    echo "Installing nginx"
+    dnf install nginx -y
+    VALIDATE nginx $?
+fi
 
 
