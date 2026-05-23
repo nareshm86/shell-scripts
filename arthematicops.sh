fa3850da04bc781d8af15f0f37eeae40fdb3254f
nareshm86 <nmarrimf2017@gmail.com>
2026-05-23 10:49:02 +0530

    all special varilables script

--- arthematicops.sh
+++ arthematicops.sh
@@ -4,6 +4,13 @@ NUM2=2
 
 SUM=$(($NUM1+$NUM2))
 
-
 echo "Total value is $SUM"
+
+MOVIES=("SRH","RCH","GT")
+
+echo "Movies are ${MOVIES($@)}
+echo "First movie is ${MOVIES[$0]}
+echo "Second Movie is ${MOVIES[$1]}
+echo "Third movie is ${MOVIES[$2]}
+echo "All movies are ${MOVIES[$@]}
 
