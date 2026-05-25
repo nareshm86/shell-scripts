d4d909d84286e55dd20922e806f29ef3428760c3
nareshm86 <nmarrimf2017@gmail.com>
2026-05-25 17:12:19 +0530

    installlog

--- mysqlinstalllog.sh
+++ mysqlinstalllog.sh
@@ -1,5 +1,5 @@
 USERID=$(id -u)
-LOGS_DIR=/var/log/shell-script
+LOGS_DIR=/var/log/shell-scripts
 LOGS_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/shell-logs/10-logs.sh.log
 
 # Check root access or not
@@ -26,15 +26,15 @@ if [ $? -eq 0 ]; then
     echo "MySQL is already installed ... SKIPPING" | tee -a $LOGS_FILE
 else
     echo "Installing MySQL"
-    dnf install mysql -y &>> $LOGS_FILE
+    dnf install mysql -y & >> $LOGS_FILE
     VALIDATE MySQL $?
 fi
 
-dnf list installed nginx &>> $LOGS_FILE
+dnf list installed nginx & >> $LOGS_FILE
 if [ $? -eq 0 ]; then
     echo "nginx is already installed ... SKIPPING" | tee -a $LOGS_FILE
 else
     echo "Installing nginx"
-    dnf install nginx -y &>> $LOGS_FILE
+    dnf install nginx -y & >> $LOGS_FILE
     VALIDATE MySQL $?
 fi
