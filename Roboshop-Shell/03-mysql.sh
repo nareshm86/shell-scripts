cf472343d2bf740c1a09c37a18c0ab43e72fb89e
nareshm86 <nmarrimf2017@gmail.com>
2026-05-29 16:00:14 +0530

    redis2

---
+++ Roboshop-Shell/03-mysql.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+{
+    echo "$TIMESTAMP [ERROR] $R Run the script with root user $N" | tee -a $LOGS_FILE
+    exit 1
+if
+}
+
