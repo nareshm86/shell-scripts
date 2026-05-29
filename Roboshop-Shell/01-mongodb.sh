fe57d62655b527292b124dc3aa910445a9d79d6d
nareshm86 <nmarrimf2017@gmail.com>
2026-05-29 14:22:01 +0530

    mongodb1

--- Roboshop-Shell/01-mongodb.sh
+++ Roboshop-Shell/01-mongodb.sh
@@ -1,43 +1,42 @@
-#!/bin/bash
+
 LOGS_FOLDER="/var/log/roboshop"
 sudo mkdir -p $LOGS_FOLDER
-sudo chmod -R 755 $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
-TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[0"
-
-USERID=$(id -u)
+N="\e[0m"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ $USERID -ne 0 ]; then
-    echo -e "$TIMESTAMP [ERROR] $R Please run script with root user $N" | tee -a $LOGS_FILE
+    echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
 VALIDATE(){
-if [ $1 - ne 0 ]; then
-    echo -e "$TIMESTAMP [ERROR] $2 $R Failed $N" | tee -a $LOGS_FILE
-    exit 1
-else
-    echo "$TIMESTAMP [INFO] $2 $G Success $N" | tee -a $LOGS_FILE
-fi
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
 }
 
-
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGS_FILE
-VALIDATE $? "Adding mongo repo"
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
 
 dnf install mongodb-org -y &>> $LOGS_FILE
-VALIDATE $? "Installing mongodb"
+VALIDATE $? "Installing MongoDB"
 
-systemctl enable --now mongod &>> $LOGS_FILE
-VALIDATE $? "Enabling & starting Mongod"
+systemctl enable --now mongod
+VALIDATE $? "Starting and enabling MongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGS_FILE
-VALIDATE $? "Allowing the remote connection to Mongodb"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
 
-systemctl restart mongod &>> $LOGS_FILE
-VALIDATE $? "restarting mongod" 
+systemctl restart mongod
+VALIDATE $? "Restarting MongoDB"
