1839ab95278ae2e94fd6583d56e2de5009c207c3
nareshm86 <nmarrimf2017@gmail.com>
2026-05-30 19:47:43 +0530

    roboshop cart shell scripts

--- Roboshop-Shell/07-cart.sh
+++ Roboshop-Shell/07-cart.sh
@@ -12,61 +12,54 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-USERID=$(id -u)
-
 if [ $USERID -ne 0 ]; then
-echo -e "$TIMESTAMP [ERROR] $R RUN the script with ROOT user $N" | tee -a $LOGS_FILE
-exit 1
+    echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
+    exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$TIMESTAMP [ERROR] $2 $R FAILED $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
-        else
-        echo -e "$TIMESTAMP [INFO] $2 $G SUCCESS $N" | tee -a $LOGS_FILE
+    else
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
-    }
+}
 
-dnf module disable nodejs -y &>> $LOGS_FILE
-dnf module enable nodejs:20 -y &>> $LOGS_FILE
-VALIDATE $? "Enable nodejs:20"
+dnf module disable nodejs -y &>>$LOGS_FILE
+dnf module enable nodejs:20 -y  &>>$LOGS_FILE
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Installing NodeJS:20"
 
-dnf install nodejs -y &>> $LOGS_FILE
-VALIDATE $? "Intsalling nodejs"
-
-id roboshop &>> $LOGS_FILE
+id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
-VALIDATE $? "creating roboshop user"
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Creating roboshop system user"
 else
-echo -e "$TIMESTAMP $G roboshop user already present $N" | tee -a $LOGS_FILE
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
 fi
 
+rm -rf /app
+VALIDATE $? "Removing existing code"
 
-rm -rf app &>> $LOGS_FILE
-VALIDATE $? "removing app directory"
+rm -rf /tmp/cart.zip
+VALIDATE $? "Removed cart zip"
 
-rm -rf /tmp/cart.zip &>> $LOGS_FILE
-VALIDATE $? "removing cart zipfile"
-
-mkdir -p /app &>> $LOGS_FILE
+mkdir -p /app  &>>$LOGS_FILE
 VALIDATE $? "Creating app directory"
 
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>> $LOGS_FILE
-cd app &>> $LOGS_FILE
-unzip /tmp/cart.zip &>> $LOGS_FILE
-VALIDATE $? "Downloading and Extracting code"
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip  &>>$LOGS_FILE
+cd /app 
+unzip /tmp/cart.zip &>>$LOGS_FILE
+VALIDATE $? "Downloaded and extracted cart code"
 
-npm install &>> $LOGS_FILE
-VALIDATE $? "Install Dependencies"
+npm install  &>>$LOGS_FILE
+VALIDATE $? "Installing dependencies"
 
-cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service &>> $LOGS_FILE
-VALIDATE $? "created cart service file"
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "Created systemctl service"
 
-systemctl daemon-reload &>> $LOGS_FILE
-systemctl enable cart  &>> $LOGS_FILE
-systemctl start cart &>> $LOGS_FILE
-VALIDATE $? "Enable and Start the cart service"
-
+systemctl enable cart &>>$LOGS_FILE
+systemctl restart cart &>>$LOGS_FILE
+VALIDATE $? "Restarting cart"
 
